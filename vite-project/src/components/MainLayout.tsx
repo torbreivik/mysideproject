73de481ccf3e5a03d244f84e80daff3d70201e00
Tor Breivik <torbreivik@Tor-sin-MacBook-Air.local>
2023-09-20 11:04:13 +0200

    Implement the matrix

--- vite-project/src/components/MainLayout.tsx
+++ vite-project/src/components/MainLayout.tsx
@@ -5,31 +5,31 @@ import { Footer } from "./Footer";
 import { columnBreakPoint } from "../utils/breakpoints";
 
 const PageWrapper = styled.main`
-    width: calc(99vw);
-    min-height: 99vh;
-    padding: 0;
-    margin: auto;
-    background-color: #111111;
+  width: calc(99vw);
+  min-height: 99vh;
+  padding: 0;
+  margin: auto;
+  background-color: #111111;
 `;
 
 const Body = styled.section`
-    display: flex;
-    flex-direction: row;
-    min-height: 88vh;
-    padding: 24px;
-    justify-content: center;
-    @media screen and (max-width: ${columnBreakPoint}px) {
-        flex-direction: column;
-        justify-content: flex-start;
-    }
+  display: flex;
+  flex-direction: row;
+  min-height: 88vh;
+  padding: 24px;
+  justify-content: center;
+  @media screen and (max-width: ${columnBreakPoint}px) {
+    flex-direction: column;
+    justify-content: flex-start;
+  }
 `;
 
 export function MainLayout() {
-    return (
-        <PageWrapper>
-            <NavBar />
-            <Body>{<Outlet />}</Body>
-            <Footer />
-        </PageWrapper>
-    );
+  return (
+    <PageWrapper>
+      <NavBar />
+      <Body>{<Outlet />}</Body>
+      <Footer />
+    </PageWrapper>
+  );
 }
--- vite-project/src/pages/Flex.tsx
+++ vite-project/src/pages/Flex.tsx
@@ -4,21 +4,38 @@ import { getScreenDimensions } from "../utils/getScreenDimensions";
 const CenterSelf = styled.div`
   display: flex;
   flex-direction: column;
-  justify-content: center;
+  /* justify-content: center; */
   align-items: center;
+  outline: solid 1px red;
+  width: 100%;
+  padding: 24px;
+  min-height: 88vh;
 `;
 
 const Headline = styled.h1``;
+const Bodytext = styled.p``;
+const Image = styled.img`
+  max-width: 600px;
+  max-height: 400px;
+  margin-bottom: 24px;
+`;
 
 export function Flex() {
   const { width, height } = getScreenDimensions();
   let headlineContent = "Agent Tor is watching you";
+  let bodytextContent = "Do you think this is reality?";
+  let imageUrl = "/public/Morpheus.png";
   if (width < 800) {
     headlineContent = "Take the red pill";
+    bodytextContent = "And take a trip down the rabbit hole";
+    imageUrl = "/public/pill.png";
   }
+
   return (
     <CenterSelf>
+      <Image src={imageUrl}></Image>
       <Headline>{headlineContent}</Headline>
+      <Bodytext>{bodytextContent}</Bodytext>
     </CenterSelf>
   );
 }
