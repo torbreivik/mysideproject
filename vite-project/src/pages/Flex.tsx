ec008caa8ed1d61f75437f51c1f8201d20dd2b9c
Tor Breivik <torbreivik@Tor-sin-MacBook-Air.local>
2023-09-20 10:22:47 +0200

    Make headline content reactive

--- vite-project/src/pages/Flex.tsx
+++ vite-project/src/pages/Flex.tsx
@@ -2,20 +2,23 @@ import styled from "styled-components";
 import { getScreenDimensions } from "../utils/getScreenDimensions";
 
 const CenterSelf = styled.div`
-    display: flex;
-    flex-direction: column;
-    justify-content: center;
-    align-items: center;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
 `;
 
 const Headline = styled.h1``;
 
 export function Flex() {
-    const { width, height } = getScreenDimensions();
-
-    return (
-        <CenterSelf>
-            <Headline>Flex</Headline>
-        </CenterSelf>
-    );
+  const { width, height } = getScreenDimensions();
+  let headlineContent = "Agent Tor is watching you";
+  if (width < 800) {
+    headlineContent = "Take the red pill";
+  }
+  return (
+    <CenterSelf>
+      <Headline>{headlineContent}</Headline>
+    </CenterSelf>
+  );
 }
